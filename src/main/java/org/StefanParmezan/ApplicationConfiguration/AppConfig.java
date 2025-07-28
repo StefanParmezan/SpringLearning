a9bb5859ecf36f73010377c23c5c58f74da17315
StefanParmezan <syromatnikov26@gmail.com>
2025-07-28 20:05:35 +0500

    An experiment was written: what is more priority for BeanFactory, normal object creation or AppConfig, conclusion: AppConfig is more priority

--- src/main/java/org/StefanParmezan/ApplicationConfiguration/AppConfig.java
+++ src/main/java/org/StefanParmezan/ApplicationConfiguration/AppConfig.java
@@ -1,5 +1,6 @@
 package org.StefanParmezan.ApplicationConfiguration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,4 +8,8 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("org.StefanParmezan.ApplicationConfiguration")
 public class AppConfig {
 
+    @Bean
+    public Entity entity() {
+        return new Entity("Entity");
+    }
 }
